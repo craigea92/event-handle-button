fbca67834c98aed21c5c812cdab64f36b83aa5b3
Craig Eaton <craigea92@gmail.com>
2023-03-14 15:32:19 +0000

    Added 2 useStates, 1 for heading text and for mouseover

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseover() {
+    setMouseOver(true);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseover}
+      >
+        Submit
+      </button>
     </div>
   );
 }
