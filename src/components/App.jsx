138363e817ab6d1ed51b0732593c42d9ff01793b
Craig Eaton <craigea92@gmail.com>
2023-03-16 13:15:20 +0000

    Added a useState for the value to be set as the heading upon submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,26 @@ import React, { useState }from "react";
 function App() {
 
   const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
 
   function handleChange(event) {
     console.log(event.target.value);
     setName(event.target.value);
   }
 
+  function handleClick() {
+    setHeading(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
+      <h1>Hello {headingText}</h1>
       <input
       onChange={handleChange} 
       type="text" 
       placeholder="What's your name?" 
       />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
