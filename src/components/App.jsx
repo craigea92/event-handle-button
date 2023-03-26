a055df4ece75733e3ac6a9584f7b390f2cf83a50
Craig Eaton <craigea92@gmail.com>
2023-03-26 14:41:36 +0100

    Experimenting with uncontrolled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,30 @@ import React, { useState }from "react";
 
 function App() {
 
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
 
-  function updateFName(event) {
-    const firstName = event.target.value;
-    setFName(firstName);
-  }
+function handleChange(event) {
+  const newValue = event.target.value;
+}
 
-  function updateLName(event) {
-    const lastName = event.target.value;
-    setLName(lastName);
-  }
 
   return (
     <div className="container">
-      <h1>Hello {fName} {lName}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" onChange={updateFName} placeholder="First Name" value={fName} />
-        <input name="lName" onChange={updateLName} placeholder="Last Name" value={lName} />
+        <input name="fName" 
+        onChange={handleChange} 
+        placeholder="First Name" 
+        // value={fullName.fName} 
+        />
+        <input name="lName" 
+        onChange={handleChange} 
+        placeholder="Last Name" 
+        // value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
