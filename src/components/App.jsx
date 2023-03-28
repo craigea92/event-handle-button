44a5929a78b7d5f93d826a06e7a4069d2d77f713
Craig Eaton <craigea92@gmail.com>
2023-03-28 19:41:36 +0100

    Uncommented comments for values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,12 +34,12 @@ function handleChange(event) {
         <input name="fName" 
         onChange={handleChange} 
         placeholder="First Name" 
-        // value={fullName.fName} 
+        value={fullName.fName} 
         />
         <input name="lName" 
         onChange={handleChange} 
         placeholder="Last Name" 
-        // value={fullName.lName}
+        value={fullName.lName}
         />
         <button>Submit</button>
       </form>
